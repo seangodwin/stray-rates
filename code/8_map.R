bcedf90aeb7962d8eb63ff7b52ec899495ad0f6a
Sean Godwin <sean.godwin@gmail.com>
2025-06-02 11:25:40 -0700

    clean up

--- code/8_map.R
+++ code/8_map.R
@@ -6,6 +6,7 @@
 
 ## 0 [LOAD PACKAGES] -----------------------------------------------------------
 library(here)        # file referencing
+library(terra)       # spatial data manipulation
 
 
 ## 1 [READ IN DATA] ------------------------------------------------------------
@@ -24,30 +25,38 @@ bath <- rast(here::here("./data/spatial/gebco_2024.tif"))
 
 ## 2 [MAP INPUTS] --------------------------------------------------------------
 # Colours for watersheds
-ws.col.full <- PNWColors::pnw_palette("Sunset", 
+col.ws.full <- PNWColors::pnw_palette("Sunset", 
                                       n=length(unique(rec.locs$ws.id))*1.2, 
                                       type="continuous")
-ws.col <- ws.col.full[1:length(unique(rec.locs$ws.id))]
-pt.col <- ws.col.full[length(ws.col.full)]
+col.ws <- col.ws.full[1:length(unique(rec.locs$ws.id))]
+col.pt <- col.ws.full[length(col.ws.full)]
+
+# Bathymetry colours
+col.bath <- rev(colorRampPalette(c("#E8F4F9", "#A3CDE2", "#31789D"))(100))
 
 # Scale river line widths based on Strahler order
 lwd.riv.bc <- scales::rescale(riv.bc$strahler, to = c(0.1, 1.5))  
 lwd.riv.us <- scales::rescale(riv.us$strahler, to = c(0.1, 1.5))
 
+# Other plot inputs
+col.land <- "grey10"
+col.border <- "grey80"   # state and province borders
+col.riv <- "white"
+col.label <- "white"
+lwd.border <- 0.1
+cex.pt <- 0.5
+cex.ctry <- 1.4
+cex.axis <- 0.8
+
 # Extent of plot
 x.range <- c(-137,-113)
 y.range <- c(37,60)
 
-# Other plot inputs
-cex.pt <- 0.5
-cex.ctry <- 1.4
 
-# Bathymetry colours
-bath.col <- rev(colorRampPalette(c("#E8F4F9", "#A3CDE2", "#31789D"))(100))
 
 
 ## 3 [MAP] ---------------------------------------------------------------------
-tiff(here::here("./plots/watersheds.tiff"), width=6, height=10, units="in",
+tiff(here::here("./plots/recovery_locations.tiff"), width=6, height=10, units="in",
      pointsize=20, res=600, compression="lzw")
 par(mar=c(0,0,0,0), tck=-0.03, mgp=c(3,0.5,0), family="sans")
   
@@ -55,46 +64,38 @@ par(mar=c(0,0,0,0), tck=-0.03, mgp=c(3,0.5,0), family="sans")
   plot(0, type="n", xlim=x.range, ylim=y.range, xaxs="i", yaxs="i", 
        axes=FALSE, xlab="", ylab="", frame.plot=FALSE)
   
-  # Background colour (for ocean)
-  par(xpd=TRUE)
-  rect(xleft = x.range[1]-10, xright = x.range[2]+10,
-       ybottom = y.range[1]-10, ytop = y.range[2]+10,
-       col = "white", border = NA)
-  par(xpd=FALSE) # reset clipping
-  
   # Bathymetry
   plot(bath, add = TRUE,
-       col = bath.col, legend=F)
+       col = col.bath, legend=F)
   
   # States and provinces
   plot(states, add=T,
-       col="grey10", border="grey80", lwd=0.1)
+       col=col.land, border=col.border, lwd=lwd.border)
   plot(provinces, add=T,
-       col="grey10", border="grey80", lwd=0.1)
+       col=col.land, border=col.border, lwd=lwd.border)
   
   # 49th parallel
   lines(x = c(-122.758, -95), y = c(49, 49), col = "white", lwd = 0.8)
   
   # Watersheds that had recoveries
   plot(ws[ws$ws.id %in% rec.locs$ws.id,], add=T,
-       col=ws.col, lwd=0.1)
+       col=col.ws, lwd=0.1)
   
   # Rivers
   plot(riv.bc, add=T,
-       lwd=lwd.riv.bc, col="white")
+       lwd=lwd.riv.bc, col=col.riv)
   plot(riv.us, add=T,
-       lwd=lwd.riv.us, col="white")
+       lwd=lwd.riv.us, col=col.riv)
   
   # Recovery locations
   plot(rec.locs, add=T, cex=cex.pt, pch=21,
-       bg=pt.col,
-       col="grey10")
+       bg=col.pt, col=col.land)
   
-  # Longitude ticks (every 5 degrees)
+  # Longitude ticks (every 5 and 1 degrees)
   axis(1, at=seq(floor(x.range[1]/5)*5, ceiling(x.range[2]/5)*5, by=5), 
-       labels=F, tck=0.025, cex.axis=0.8)
+       labels=F, tck=0.025, cex.axis=cex.axis)
   axis(1, at=seq(floor(x.range[1]/5)*5, ceiling(x.range[2]/5)*5, by=1), 
-       labels=F, tck=0.015, cex.axis=0.8)
+       labels=F, tck=0.015, cex.axis=cex.axis)
   
   # Latitude ticks (every 5 and 1 degrees)
   axis(2, at=seq(floor(y.range[1]/5)*5, ceiling(y.range[2]/5)*5, by=5), 
@@ -103,8 +104,10 @@ par(mar=c(0,0,0,0), tck=-0.03, mgp=c(3,0.5,0), family="sans")
        labels=F, tck=0.01, cex.axis=0.8)
   
   # Country labels
-  text(x=-120, y=56, labels = "Canada", family="sans", cex=cex.ctry, col="white")
-  text(x=-117, y=40, labels = "USA", family="sans", cex=cex.ctry, col="white")
+  text(x=-120, y=56, labels = "Canada", family="sans", 
+       cex=cex.ctry, col=col.label)
+  text(x=-117, y=40, labels = "USA", family="sans", 
+       cex=cex.ctry, col=col.label)
   
   # Boundary box
   box()
